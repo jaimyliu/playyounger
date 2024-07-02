36de3b0cc7d3cedd62208c29f30830b03db817a8
Jaimy Liu <158254278+jaimyliu@users.noreply.github.com>
2024-07-03 03:41:21 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,21 +1,16 @@
-import sys
-import os
 from flask import Flask
 from flask_socketio import SocketIO
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+socketio = None
 
-# 创建 Flask 应用程序实例
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key_here'
 
-# 创建 SocketIO 实例，并将其与应用程序绑定
-socketio = SocketIO(app)
+    global socketio
+    socketio = SocketIO(app)
 
-# 导入并注册前端蓝图
-from frontend.views import frontend_bp
-app.register_blueprint(frontend_bp)
+    from .views import frontend_bp
+    app.register_blueprint(frontend_bp)
 
-# 导入并注册后端蓝图
-from flask_game import backend
-from .frontend.views import frontend_bp
-app.register_blueprint(backend, url_prefix='/backend')
+    return app
